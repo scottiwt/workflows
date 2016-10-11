0885d3e59a77f97242682d27913a8fa61880e692
scottiwt <iwt@integritywebtechnology.com>
2016-10-11 05:43:39 -0700

    Added Static and JSON to watch monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
     concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+
 var jsSources = [
   'components/scripts/rclick.js',
   'components/scripts/pixgrid.js',
@@ -14,6 +15,8 @@ var jsSources = [
   'components/scripts/template.js'
 ];
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
 gulp.task('coffee', function() {
   gulp.src(coffeeSources)
@@ -46,6 +49,8 @@ gulp.task('watch', function(){
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
+  gulp.watch(htmlSources, ['html']);
+  gulp.watch(jsonSources, ['json']);
 
 });
 
@@ -56,4 +61,15 @@ gulp.task('connect', function(){
   });
 
 });
-gulp.task('default', ['coffee', 'js', 'compass','connect', 'watch']);
+
+gulp.task('html', function(){
+  gulp.src(htmlSources)
+  .pipe(connect.reload())
+});
+
+gulp.task('json', function(){
+  gulp.src(jsonSources)
+  .pipe(connect.reload())
+});
+
+gulp.task('default', ['html','coffee', 'js', 'compass','connect', 'watch']);
